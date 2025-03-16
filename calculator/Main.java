f1a65fb6f30a9c18ccd7c35eaaf96f8f326ce8df
Miłosz Meller <git@milosz.me>
2025-03-16 16:34:06 +0100

    Finish Stage 2

--- calculator/Main.java
+++ calculator/Main.java
@@ -3,11 +3,23 @@ package calculator;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int first = scanner.nextInt();
-        int second = scanner.nextInt();
-        System.out.println(first + second);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("/exit")) {
+                break;
+            } else if (input.isEmpty()) {
+                continue;
+            } else if (!input.contains(" ")) {
+                System.out.println(input);
+            } else {
+                String[] inputParts = input.split(" ");
+                int first = Integer.parseInt(inputParts[0]);
+                int second = Integer.parseInt(inputParts[1]);
+                System.out.println(first + second);
+            }
+        }
+        System.out.println("Bye!");
     }
 }
